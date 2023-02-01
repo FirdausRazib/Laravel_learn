d33ccddab76baf9f432719c08934a627da22b1c3
firdausrazib <firdausrazib06@egmail.com>
2023-02-01 11:13:45 +0800

    add new bootstrap

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en" data-layout-mode="detached" data-topbar-color="dark" data-sidenav-color="light"
+    data-sidenav-user="true">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Dashboard | Hyper - Responsive Bootstrap 5 Admin Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+
+    <!-- Icons css -->
+    <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+
+    <!-- App css -->
+    <link href="{{asset('assets/css/app-modern.min.css')}}" rel="stylesheet" type="text/css" id="app-style" />
+</head>
+
+<body>
+    <!-- Begin page -->
+    <div class="wrapper">
+        <!-- ========== Topbar Start ========== -->
+        @include('layouts.topbar.index')
+        <!-- ========== Topbar End ========== -->
+
+        <!-- ========== Left Sidebar Start ========== -->
+        @include('layouts.sidebar.index')
+        <!-- ========== Left Sidebar End ========== -->
+
+        <!-- ============================================================== -->
+        <!-- Start Page Content here -->
+        <!-- ============================================================== -->
+
+        <div class="content-page">
+            <div class="content">
+                @yield('content')
+            </div>
+            <!-- content -->
+
+            <!-- Footer Start -->
+            @include('layouts.footer.index')
+            <!-- end Footer -->
+        </div>
+
+        <!-- ============================================================== -->
+        <!-- End Page content -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- END wrapper -->
+
+    <!-- Theme Settings -->
+    @include('layouts.themes.index')
+    <!-- End Theme Settings -->
+
+    <!-- Vendor js -->
+    <script src="{{asset('assets/js/vendor.min.js')}}"></script>
+
+    <!-- App js -->
+    <script src="{{asset('assets/js/app.min.js')}}"></script>
+</body>
+
+</html>
